7db23e494b83fba90aa6765cef3b0474b3b95175
versalitas <100954079+versalitas@users.noreply.github.com>
2022-06-07 10:40:08 +0200

    Update concise_code.js

--- concise_code.js
+++ concise_code.js
@@ -10,3 +10,11 @@ const infoAnimal = (animal) => {
 const {name, colour, legs} = animal;
       console.log(`The animal is a ${name} with ${colour} fur.);
 }
+
+
+//constucting a new obeject out of two or several
+
+const pikachu = {name: pikachu};
+const stats = {force:3, colour:yellow};
+const pikachuStats = (...pikachu...stats);
+
