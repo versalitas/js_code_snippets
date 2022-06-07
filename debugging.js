7acbf9548a03180da5c899e3b8cd3b107d74f870
versalitas <100954079+versalitas@users.noreply.github.com>
2022-06-07 10:18:54 +0200

    Update debugging.js

--- debugging.js
+++ debugging.js
@@ -7,3 +7,18 @@ console.log({foo, bar, foobar});
 let someArray = [2,3,5];
 
 console.table(someArray);
+
+//benchmarking performance... defining timer as looper
+
+console.time('looper');
+
+let i = 0; 
+
+while (i < 10000){
+i++;
+}
+console.timeEnd('looper');
+
+//checking where a function was called
+
+deleteFunction = () => console.trace('Called here');
